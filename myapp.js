befb1a51922def87e3a31e127461feeda4cd2a74
paccymegastar07 <abayisengaaimepacifique@gmail.com>
2023-02-04 12:09:51 +0200

    changes made to the files

--- myapp.js
+++ myapp.js
@@ -1,11 +1,11 @@
 const express=require('express')
-const cors=require('cors')
 const app=express();
 const path=require('path')
 const multer=require('multer') 
 const mysql=require('mysql')
 const { diskStorage}=require('multer')
-const dotenv=require('dotenv')
+const dotenv=require('dotenv');
+;
 dotenv.config();
 
     const connection=mysql.createConnection({
@@ -20,7 +20,7 @@ connection.connect((err)=>{
         console.log(err.message);
     }
     console.log('db '+ connection.state);
-})
+});
 
 
 const storage=diskStorage({
@@ -40,9 +40,16 @@ app.use(express.urlencoded({extended:false}));
 app.use(express.static(path.join(__dirname,"images")))
  
 
+app.set('view engine','ejs')
+
+
 
 app.get('/',(req,res)=>{
     res.sendFile(__dirname + '/myapp.html')
+  
+})
+app.get('/login',(req,res)=>{
+    res.sendFile("./loggedin.html",{root:__dirname})
 })
 
 let image;
@@ -63,22 +70,46 @@ app.post('/add-data',upload.single('image'),(req,res)=>{
           if (err) console.log("err found:", err);
         //   res.send("data saved in database successfully")
           console.log("data entered successfully!");
+          res.redirect('/login')
         }
       );
-      
-      connection.query(`SELECT * FROM credentials WHERE username = ? AND password =? `,[username,password],async(err,result)=>{
-        if(err) console.log("err found");
-       
-            // image=path.join("images",result[0].image.toString())
-          image = path.join(__dirname + 'images',result[0].image)
-        //   console.log(image);
-        //   res.sendFile(__dirname + '/myapp.html')
+})
+app.get('/login',(req,res)=>{
+    res.sendFile("./loggedin.html",{root:__dirname})
+})
 
-        res.json({image:image,uname:username,email:email})
-        
-      })
+app.post('/login',(req,res)=>{
 
- })
+    let password=req.body.password;
+    let username=req.body.name
+    
+ let query=`SELECT * FROM credentials WHERE username= ? AND password=?`
+    connection.query(query,[username,password],(err,result)=>{
+        if(err)throw err
+        else if(result.length>0){
+            for(let i=0;i<result.length;i++){
+            if(result[i].username==username && result[i].password==password){
+                res.redirect('/update')
+            
+            }
+        }
+        }
+        else{
+            res.send("sorry")
+        }
+
+    })
+})
+
+app.get('/update',(req,res)=>{
+    res.sendFile(path.join(__dirname,"./update.html"))
+})
+app.post('/update',(req,res)=>{
+    let password=req.body.password;
+    let username=req.body.name
+    let email=req.body.email
+    connection.query(`UPDATE credentials SET username=${username} ,password=${password} ,email=${email}`)
+})
 
 
 app.delete('/delete/:id',(req,res)=>{
@@ -102,28 +133,6 @@ app.delete('/delete/:id',(req,res)=>{
 })
 
 
-app.put('/update/:id',(req,res)=>{
-    console.log(req.body);
-   let id=req.params.id
-   let password=req.body.password;
-   let username=req.body.name
-   let email=req.body.email
-   
-
-
-    connection.query(`UPDATE credentials SET password= ?,email= ? WHERE id= ?`,[password,email,id],(err,result)=>{
-        if(err) {console.log(err);
-    }
-    else{
-     if(result.affectedRows==0){
-        res.send("Id not present")
-     }
-     else{
-        res.send("updated")
-     }
-    }
-    })
-})
 const port=process.env.PORT || 3500
 app.listen(3500,()=>{
     console.log(`the app is running `);
